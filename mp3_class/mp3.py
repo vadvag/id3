2a6b7b252c86f47ee93251fbf0cb81452874bcb8
maxudovenko <max.udovenko@gmail.com>
2017-12-10 21:03:30 +0200

    class MP3Format handle the file header

--- mp3_class/mp3.py
+++ mp3_class/mp3.py
@@ -1,4 +1,6 @@
 import os
+import sys
+import struct
 
 class MP3Format:
     """ Class-description of mp3 file
@@ -8,11 +10,55 @@ class MP3Format:
     """
     
     def __init__(self, path):
-        # self.name = name + '.mp3'
-        self.path = path
+        self.full_path = path
+        self.meta_data = {}
 
-    version = ['ID3v1', 'ID3v2']
+        # define variables for File Name and Path to directory that contains this file
+        if sys.platform == 'darwin' or sys.platform == 'linux':
+            self.name = self.full_path.rsplit('/')[-1]
+            self.path = self.full_path.rsplit('/')[0]
+        else:
+            # o_O do U use Windows? :-)
+            self.name = self.full_path.rsplit('\\')[-1]
+            self.path = self.full_path.rsplit('\\')[0]
 
+        self.parse_id3_tags()
 
-# Path to current directory
-# https://docs.python.org/3/library/os.html#module-os
+    def parse_id3_tags(self):
+        with open(self.full_path, 'rb') as fd:
+            # Check if our file version is ID3v2
+            # 3 bytes: v2 header is "ID3"
+            __ID3v = str(fd.read(3))
+            if  __ID3v == "ID3":
+                
+                # 2 bytes: the minor and revision versions.
+                __m_and_r_versions = struct.unpack("h", fd.read(2))
+    
+                if len(__m_and_r_versions) == 1:
+                    self.meta_data["version"] = '{}v{}.0'.format(__ID3v, __m_and_r_versions[0])
+                else:
+                    self.meta_data["version"] = '{}v{}.{}'.format(__ID3v, 
+                                                                __m_and_r_versions[0], 
+                                                                __m_and_r_versions[1])
+
+                # 1 byte (first 4 bits): flags
+                self.meta_data["flags"] = '{0:08b}'.format(ord(fd.read(1)))
+                """
+                http://id3.org/id3v2.4.0-structure
+                flags
+                a - Unsynchronisation
+                b - Extended header
+                c - Experimental indicator
+                d - Footer present
+                """
+                
+                # 4 bytes: The size of the extended header (if any), frames, and padding
+                # afer unsynchronization. This is a sync safe integer, so only the
+                # bottom 7 bits of each byte are used.
+                self.meta_data["extended_header"] = fd.read(4)
+
+                # self.meta_data['frame_1_header'] = fd.read(256)
+                # self.meta_data['frame_1_size'] = fd.read(2)
+            else:
+                # temporary empty, will be used for handle formats < 2 version
+                pass
