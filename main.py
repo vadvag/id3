f0b8c0bd76ce8d0e10787f005352b5a422218ff8
maxudovenko <max.udovenko@gmail.com>
2017-12-12 13:46:27 +0200

    main.py modifyed

--- main.py
+++ main.py
@@ -5,22 +5,22 @@ import os
 
 # Этот кусок кода нам нужен пока мы не прошли тему Модули, потом будем прописывать свои модули по-человечески
 from settings import set_path_vars
-set_path_vars()
-
 import search_in_directory as sid
 
-# Временная мера на тот случае если файл будет запускаться не с коммандной строки и без параметров
-if len(sys.argv) == 1:
-    exit(1)
-
-# предполагается что на данном этапе наше приложение получает один аргумент коммандной строки
-# протестировано:  python3.6 main.py /Users/MacUser/Music
-# В результате выдает список всех mp3 файлов в заданной директории
-if os.path.exists(os.path.dirname(sys.argv[1])):
-    my_mp3 = sid.find_all_mp3_in_current_dir(sys.argv[1])
+try:
+    # предполагается что на данном этапе наше приложение получает один аргумент коммандной строки
+    # протестировано: python3.6 main.py /Users/MacUser/Music
+    # В результате выдает список всех mp3 файлов в заданной директории с версиями и тегов
     
-    for mp3_file in my_mp3:
-        print('mp3 version {}: {}'.format(mp3_file.meta_data['version'], mp3_file.full_path))
+    if os.path.exists(os.path.dirname(sys.argv[1])):
+        my_mp3 = sid.find_all_mp3_in_current_dir(sys.argv[1])
+        
+        for mp3_file in my_mp3:
+            print('mp3 version {}: {}'.format(mp3_file.meta_data['version'], mp3_file.full_path))
 
-else:
-    print("Not OK")
+    else:
+        print("Path [{}] does not exist".format(sys.argv[1]))
+
+# если файл будет запускаться не с коммандной строки и/или без параметров
+except IndexError:
+    print('Path parameter is empty, please restart the application and set the Path to directory with music')
--- settings.py
+++ settings.py
@@ -4,10 +4,14 @@ import sys
 import os
 
 # please set here the names of directories that contains your modules
-id3_modules = ( 'search',
-            'mp3_class'
-            )
+id3_modules = (
+    'search',
+    'mp3_class'
+    )
 
 def set_path_vars():
     for item in id3_modules:
         sys.path.append(os.path.join(sys.path[0], item))
+
+
+set_path_vars()
